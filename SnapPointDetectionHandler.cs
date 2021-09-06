844fdf5b529a5d638b057091866b6e5f86e79fff
ScriptingGuru <86731296+ScriptingGuru@users.noreply.github.com>
2021-09-06 10:39:59 +0200

    Delete SnapPointDetectionHandler.cs

--- SnapPointDetectionHandler.cs
+++
@@ -1,7 +0,0 @@
-public static class SnapPointDetectionHandler
-{
-    public static SnapPointTrigger currentSELECTEDSnapPoint;
-    public static SnapPointTrigger currentDETECTEDSnapPoint;
-    private static float closestDistance;
-    public static float ClosestDistance { get => closestDistance; set => closestDistance = value; }
-}

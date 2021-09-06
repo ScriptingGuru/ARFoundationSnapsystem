2b1d912871ecb554f0c705547e8f5ea2a9e2229f
ScriptingGuru <86731296+ScriptingGuru@users.noreply.github.com>
2021-09-06 10:43:14 +0200

    Update SnapPointDetectionController.cs

--- SnapPointDetectionController.cs
+++ SnapPointDetectionController.cs
@@ -1,26 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SnapPointDetectionController : MonoBehaviour
+public static class SnapPointDetectionHandler
 {
-    private static SnapPointDetectionController instance;
-    public static SnapPointDetectionController Instance { get => instance; }
-    private static bool triggerZoneDetected;
-    public bool TriggerZoneDetected { get => triggerZoneDetected; set => triggerZoneDetected = value; }
-
-    public SnapPointTrigger currentSELECTEDSnapPoint;
-    public SnapPointTrigger currentDETECTEDSnapPoint;
-
-
-    private void Awake()
-    {
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-            return;
-        }
-
-        instance = this;
-    }
+    public static SnapPointTrigger currentSELECTEDSnapPoint;
+    public static SnapPointTrigger currentDETECTEDSnapPoint;
+    private static float closestDistance;
+    public static float ClosestDistance { get => closestDistance; set => closestDistance = value; }
 }

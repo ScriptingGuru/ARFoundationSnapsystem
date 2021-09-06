c4a91b825744f03aae0d9917feebd8d4cf1d21b7
ScriptingGuru <86731296+ScriptingGuru@users.noreply.github.com>
2021-09-06 10:43:43 +0200

    Update GhostVerifySnapPosition.cs

--- GhostVerifySnapPosition.cs
+++ GhostVerifySnapPosition.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostCollisionVerfication : MonoBehaviour
+public class GhostVerifySnapPosition : MonoBehaviour
 {
     [SerializeField] private SnapSystem snapTriggerSystem;
     [SerializeField] private CollisionDetecter collisionDetecter;
